5ed2432e7e2f8192588b67a597381841ede4657e
Maurizio Tomasi <maurizio.tomasi@unimi.it>
2018-02-13 18:31:53 +0100

    Add more documentation

--- src/Healpix.jl
+++ src/Healpix.jl
@@ -482,6 +482,12 @@ end
 
 ################################################################################
 
+"""
+    savePixelsToFITS(map::Map{T}, f::FITSIO.FITSFile, column) where {T <: Number}
+
+Save the pixels of `map` into the column with index/name `column` in the FITS
+file, which must have been already opened.
+"""
 function savePixelsToFITS(map::Map{T},
                           f::FITSIO.FITSFile,
                           column) where {T <: Number}
@@ -531,6 +537,16 @@ function saveToFITS(map::Map{T, NestedOrder},
 
 end
 
+"""
+    saveToFITS(map::Map{T, O}, filename::AbstractString, typechar="D", unit="", extname="MAP") where {T <: Number, O <: Order}
+
+Save a map into a FITS file. The name of the file is specified in `filename`; if it begins with `!`,
+existing files will be overwritten without warning. The parameter `typechar` specifies the data type
+to be used in the FITS file: the default (`D`) will save 64-bit floating-point values. See the
+CFITSIO documentation for other values. The keyword `unit` specifies the measure unit used for the
+pixels in the map. The keyword `extname` specifies the name of the HDU where the map pixels will
+be written.
+"""
 function saveToFITS(map::Map{T, O},
                     fileName::AbstractString;
                     typechar="D",
