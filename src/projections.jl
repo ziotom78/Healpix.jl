9d8f317878d47f24d74e565ec24bc76809feb5db
Maurizio Tomasi <ziotom78@gmail.com>
2019-04-18 15:35:31 +0200

    Implement the gnomonic projection

--- src/projections.jl
+++ src/projections.jl
@@ -4,7 +4,8 @@ export UNSEEN,
     lat2colat, colat2lat,
     project,
     equiprojinv, mollweideprojinv, orthoinv,
-    equirectangular, mollweide, orthographic, orthographic2
+    equirectangular, mollweide, orthographic,
+    orthographic2, gnomonic
 
 import RecipesBase
 
@@ -113,9 +114,7 @@ function mollweideprojinv(x, y; kwargs...)
     #
     # x ∈ [-1, 1], y ∈ [-1, 1]
 
-    if x^2 + y^2 ≥ 1
-        return (false, 0, 0)
-    end
+    x^2 + y^2 ≥ 1 && return (false, 0.0, 0.0)
 
     sinθ = y
     cosθ = sqrt(1 - sinθ^2)
@@ -144,13 +143,13 @@ function orthoinv(x, y, ϕ1, λ0; kwargs...)
     R = 1
     ρ = √(x^2 + y^2)
     if ρ > R
-        return (false, 0, 0)
+        return (false, zero(ϕ1), zero(λ0))
     end
     
     c = asin(ρ / R)
     sinc, cosc = sin(c), cos(c)
     if cosc < 0
-        return (false, 0, 0)
+        return (false, zero(ϕ1), zero(λ0))
     end
     
     if ρ ≈ 0
@@ -183,6 +182,46 @@ function ortho2inv(x, y, ϕ1, λ0; kwargs...)
     orthoinv(2x - 1, y, ϕ1, λ0 + π)
 end
 
+"""
+    function gnomonicinv(x, y, ϕ1, λ0, fov_rad)
+
+Gnomonic projection centered on (ϕ1, λ0), with a field of view
+equal to `fov_rad` (in radians).  Given a point (x, y) on the plane,
+with x ∈ [-1, 1], y ∈ [-1, 1], return a 3-tuple of type (Bool,
+Number, Number). The boolean specifies if (x, y) falls within
+the map (true) or not (false), the second and third arguments
+are the latitude and longitude in radians.
+"""
+function gnominv(x, y, ϕ1, λ0, ψ0, fov_rad; kwargs...)
+    # We fix a Earth radius such that the field of view of the
+    # projection is the one expected. Note that the formula
+    # diverges if `fov_rad` is 90° (as expected).
+    R = 1. / tan(fov_rad)
+
+    # We use basic geometry to cast a 3D ray from the center of
+    # the sphere to the tangent plane, and then we compute the
+    # intersection between the ray and the sphere.
+    gamma = 1 / sqrt(1 + (x^2 + y^2) / R^2)
+    vecx, vecy, vecz = gamma * R, -gamma * x, -gamma * y
+
+    sin_ϕ1, cos_ϕ1 = sincos(ϕ1)
+    sin_λ0, cos_λ0 = sincos(λ0)
+    sin_ψ0, cos_ψ0 = sincos(-ψ0) # Change the sign to match Healpy conventions
+    
+    vecxrot = (cos_ϕ1 * cos_λ0 * vecx +
+               (-cos_ψ0 * cos_ϕ1 * sin_λ0 + sin_ψ0 * sin_ϕ1) * vecy + 
+               (-sin_ψ0 * cos_ϕ1 * sin_λ0 + sin_ϕ1 * cos_ψ0) * vecz)
+    vecyrot = ((sin_λ0) * vecx +
+               (cos_ψ0 * cos_λ0 + sin_ψ0 * sin_ϕ1 * sin_λ0) * vecy +
+               (-sin_ψ0 * cos_λ0) * vecz)
+    veczrot = ((-sin_ϕ1 * cos_λ0) * vecx +
+               (sin_ψ0 * sin_ϕ1 * sin_λ0 + cos_ϕ1 * sin_ψ0) * vecy +
+               (-sin_ψ0 * sin_ϕ1 * sin_λ0 + cos_ϕ1 * cos_ψ0) * vecz)
+    
+    theta, phi = vec2ang(vecxrot, vecyrot, veczrot)
+    (true, colat2lat(theta), phi)
+end
+
 ################################################################################
 
 """
@@ -197,9 +236,15 @@ function equirectangular(m::Map{T,O}, projparams = Dict()) where {T <: Number,O 
 end
 
 """
-    mollweide(m::Map{T,O}; kwargs...) where {T <: Number, O <: Order}
+    mollweide(m::Map{T,O}, projparams = Dict()) where {T <: Number,O <: Order}
 
 High-level wrapper around `project` for Mollweide projections.
+
+The following parameters can be set in the `projparams` dictionary:
+
+- `width`: width of the image, in pixels (default: 720 pixels)
+- `height`: height of the image, in pixels; if not specified, it will be assumed
+  to be equal to `width`
 """
 function mollweide(m::Map{T,O}, projparams = Dict()) where {T <: Number,O <: Order}
     width = get(projparams, :width, 720)
@@ -208,9 +253,17 @@ function mollweide(m::Map{T,O}, projparams = Dict()) where {T <: Number,O <: Ord
 end
 
 """
-    orthographic(m::Map{T,O}, ϕ0, λ0; kwargs...) where {T <: Number, O <: Order}
+    orthographic(m::Map{T,O}, projparams = Dict()) where {T <: Number,O <: Order}
 
-High-level wrapper around `project` for orthographic projections centered around the point (ϕ0, λ0).
+High-level wrapper around `project` for orthographic projections.
+
+The following parameters can be set in the `projparams` dictionary:
+
+- `width`: width of the image, in pixels (default: 720 pixels)
+- `height`: height of the image, in pixels; if not specified, it will be assumed
+  to be equal to `width`
+- `center`: position of the pixel in the middle of the left globe (*latitude* and
+  longitude).
 """
 function orthographic(m::Map{T,O}, projparams = Dict()) where {T <: Number,O <: Order}
     width = get(projparams, :width, 720)
@@ -222,17 +275,59 @@ function orthographic(m::Map{T,O}, projparams = Dict()) where {T <: Number,O <: 
 end
 
 """
-    orthographic2(m::Map{T,O}, ϕ0, λ0; kwargs...) where {T <: Number, O <: Order}
+    orthographic2(m::Map{T,O}, projparams = Dict()) where {T <: Number,O <: Order}
 
-High-level wrapper around `project` for stereo orthographic projections centered
-around the point (ϕ0, λ0).
+High-level wrapper around `project` for stereo orthographic projections.
+
+The following parameters can be set in the `projparams` dictionary:
+
+- `width`: width of the image, in pixels (default: 720 pixels)
+- `height`: height of the image, in pixels; if not specified, it will be assumed
+  to be equal to `width`
+- `center`: position of the pixel in the middle of the left globe (*latitude* and
+  longitude). Default is (0, 0).
 """
 function orthographic2(m::Map{T,O}, projparams = Dict()) where {T <: Number,O <: Order}
     width = get(projparams, :width, 720)
     height = get(projparams, :height, width ÷ 2)
     ϕ0, λ0 = get(projparams, :center, (0, 0))
     project(m, width, height, projparams) do x, y
-    ortho2inv(x, y, ϕ0, λ0)
+        ortho2inv(x, y, ϕ0, λ0)
+    end
+end
+
+"""
+    gnomonic(m::Map{T,O}, projparams = Dict()) where {T <: Number,O <: Order}
+
+High-level wrapper around `project` for gnomonic projections.
+
+The following parameters can be set in the `projparams` dictionary:
+
+- `width`: width of the image, in pixels (default: 720 pixels)
+- `height`: height of the image, in pixels; if not specified, it will be assumed
+  to be equal to `width`
+- `center`: position and orientation of the pixel in the middle. It is a 3-element
+  tuple containing:
+
+  1. The *latitude* of the pixel, in radians
+  2. The longitude of the pixel, in radians
+  3. The rotation to be applied to the image, in radians
+
+- `fov_rad`: size of the image along the x and y axes, in radians (default: 15°)
+
+# Example
+
+````julia
+plot(m, gnomonic, Dict(:fov_rad = deg2rad(1.5), :center = (0, 0, deg2rad(45))))
+````
+"""
+function gnomonic(m::Map{T,O}, projparams = Dict()) where {T <: Number,O <: Order}
+    width = get(projparams, :width, 720)
+    height = get(projparams, :height, width)
+    ϕ0, λ0, ψ0 = get(projparams, :center, (0, 0, 0))
+    fov_rad = get(projparams, :fov_rad, deg2rad(15 / 60))
+    project(m, width, height, projparams) do x, y
+        gnominv(x, y, ϕ0, λ0, ψ0, fov_rad)
     end
 end
 
@@ -305,16 +400,19 @@ bitmap. Possible values for `projection` are the following:
 - `equirectangular`
 - `mollweide`
 - `orthographic`
+- `orthographic2`
+- `gnomonic`
 
-You can define your own projections, if you wish.
+You can define your own projections.
 
 The dictionary `projparams` allows to hack a number of parameters used
 in the projection.
 
 # References
 
-See also [`equirectangular`](@ref), [`mollweide`](@ref), and
-[`orthographic`](@ref).
-
+See also [`equirectangular`](@ref), [`mollweide`](@ref),
+[`orthographic`](@ref), [`orthographic2`](@ref), and
+[`gnomonic`](@ref).
 """
 plot
+
