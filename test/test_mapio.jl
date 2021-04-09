26655681b01f93c7da893188684e353541ae39b1
xzackli <xzackli@gmail.com>
2021-04-09 00:30:24 -0400

    move tests to mapio

--- test/test_mapio.jl
+++ test/test_mapio.jl
@@ -17,3 +17,18 @@ print("Saving $mapFileName\n")
 Healpix.saveToFITS(m, "!$mapFileName", typechar = "I")
 m2 = Healpix.readMapFromFITS(mapFileName, 1, Int8)
 @test m.pixels == m2.pixels
+
+
+# Map reordering
+m_ring = Healpix.readMapFromFITS("float_map.fits", 1, Float32)
+m_nest = Healpix.readMapFromFITS("float_map_nest.fits", 1, Float32)
+@test typeof(m_ring) == Healpix.Map{Float32,Healpix.RingOrder,Array{Float32,1}}
+@test typeof(m_nest) == Healpix.Map{Float32,Healpix.NestedOrder,Array{Float32,1}}
+
+m_converted = Healpix.nest2ring(m_nest)
+@test all(m_converted.pixels .≈ m_ring.pixels)
+m_reconverted = Healpix.ring2nest(m_converted)
+@test all(m_reconverted.pixels .≈ m_nest.pixels)
+
+m_converted = Healpix.ring2nest(m_ring)
+@test all(m_converted.pixels .≈ m_nest.pixels)
