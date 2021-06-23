40192ee24d4ba292e9b085735fc5d98a8ea28c45
Maurizio Tomasi <ziotom78@gmail.com>
2021-06-23 07:53:08 +0200

    Add support for Union{Nothing, T} maps

--- test/test_mapio.jl
+++ test/test_mapio.jl
@@ -32,3 +32,55 @@ m_reconverted = Healpix.ring2nest(m_converted)
 
 m_converted = Healpix.ring2nest(m_ring)
 @test all(m_converted.pixels .≈ m_nest.pixels)
+
+# Nothingness/UNSEEN on a *temperature* map
+
+# Create a map with no data associated to pixels
+m_nothing = Healpix.HealpixMap{Union{Float32, Nothing}, Healpix.RingOrder}(1)
+for i in length(m_nothing)
+    @test isnothing(m_nothing[i])
+end
+
+nothing_name = tempname(; cleanup=false)
+Healpix.saveToFITS(m_nothing, nothing_name)
+
+m_read = Healpix.readMapFromFITS(nothing_name, 1, Union{Float32, Nothing})
+@test length(m_read) == length(m_nothing)
+for i in length(m_read)
+    @test isnothing(m_read[i])
+end
+
+# Not using Union{Nothing, T} means that we get UNSEEN pixels
+m_unseen_read = Healpix.readMapFromFITS(nothing_name, 1, Float32)
+for i in length(m_unseen_read)
+    @test m_unseen_read[i] ≈ Healpix.UNSEEN
+end
+
+# Nothingness/UNSEEN on a *polarization* map
+
+# Create a map with no data associated to pixels
+m_nothing = Healpix.PolarizedHealpixMap{Union{Float32, Nothing}, Healpix.RingOrder}(1)
+for i in length(m_nothing.i)
+    @test isnothing(m_nothing.i[i])
+    @test isnothing(m_nothing.q[i])
+    @test isnothing(m_nothing.u[i])
+end
+
+nothing_name = tempname(; cleanup=false)
+Healpix.saveToFITS(m_nothing, nothing_name)
+
+m_read = Healpix.readPolarizedMapFromFITS(nothing_name, 1, Union{Float32, Nothing})
+@test length(m_read.i) == length(m_nothing.i)
+for i in length(m_read.i)
+    @test isnothing(m_read.i[i])
+    @test isnothing(m_read.q[i])
+    @test isnothing(m_read.u[i])
+end
+
+# Not using Union{Nothing, T} means that we get UNSEEN pixels
+m_unseen_read = Healpix.readPolarizedMapFromFITS(nothing_name, 1, Float32)
+for i in length(m_unseen_read.i)
+    @test m_unseen_read.i[i] ≈ Healpix.UNSEEN
+    @test m_unseen_read.q[i] ≈ Healpix.UNSEEN
+    @test m_unseen_read.u[i] ≈ Healpix.UNSEEN
+end
