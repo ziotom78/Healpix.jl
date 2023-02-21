38c7d6979f8721fa6431dcb08d23ffee159a6f5d
Aurelio Amerio <43349899+aurelio-amerio@users.noreply.github.com>
2023-02-21 17:05:54 +0100

    tests for getAllNeighbours

--- test/test_pixelfunctions.jl
+++ test/test_pixelfunctions.jl
@@ -2959,3 +2959,15 @@ test_map = Healpix.HealpixMap{Float64, Healpix.RingOrder}([Float64(i) for i in 1
 @test Healpix.getRingPixels(test_map, 2) == Vector{Float64}(5:12)
 @test Healpix.getRingPixels(test_map, 5) == Vector{Float64}(29:36)
 @test Healpix.getRingPixels(test_map, 7) == Vector{Float64}(45:48)
+
+# test neighbour pixels
+let
+    resol = Healpix.Resolution(1024)
+    pix = 42
+    neighbour_pixels_ring = Healpix.getAllNeighbours(42, resol)
+    @test neighbour_pixels_ring ==  [62,41,25,13,26,43,63,87]
+    
+    neighbour_pixels_nest = Healpix.getAllNeighbours(42, resol, nest=true)
+    @test neighbour_pixels_nest ==  [41,43,44,47,45,39,36,35]
+end
+
