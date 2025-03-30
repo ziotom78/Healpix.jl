c92f4b775ce5e900026fddfaff1b4dc82ccd62eb
abhro <5664668+abhro@users.noreply.github.com>
2025-03-30 19:30:52 -0400

    Remove `@doc` from docstring

--- src/mapmaking.jl
+++ src/mapmaking.jl
@@ -1,6 +1,6 @@
 export tod2map, combinemaps!
 
-@doc raw"""
+"""
     tod2map{T,O}(pixidx, tod::Array{T}; nside=128) :: (map, hits)
 
 Create a binned map for a TOD and return a tuple containing the map
